decf1401a81aa78ccec4f25ae7cba2bb41941820
mericakay <mericakay@gmail.com>
2017-11-20 19:08:04 +0300

    Gün Sonu

--- OkulSiS_Mobile/OkulSiS_Mobile/www/scripts/devamsizlik/yakin.js
+++ OkulSiS_Mobile/OkulSiS_Mobile/www/scripts/devamsizlik/yakin.js
@@ -11,7 +11,7 @@ function user() {
             $(this).children('i:last-child').toggleClass('fa-caret-down fa-caret-left');
         }
     });
- 
+
     var okulid = localStorage.getItem("OkulID");
     var kisiid = localStorage.getItem("gelenid");
     var dersyiliid = localStorage.getItem("dyiliid");
@@ -21,8 +21,8 @@ function user() {
     var rolid = localStorage.getItem("RolID");
     var ip = localStorage.getItem("proxy");
     var kisiadi = localStorage.getItem("KullaniciAdi");
-   
-   
+
+
 
     //menu başlangıç
 
@@ -32,7 +32,7 @@ function user() {
         type: 'GET',
         dataType: 'json',
         success: function (data) {
-           
+
             var j;
             var len = data.length;
             var dataSet = [];
@@ -47,11 +47,11 @@ function user() {
                 iconclass = data[j].iconclass;
                 collapse = data[j].collapse;
                 // alert(collapse);
-           
 
-                    $('.left').append('<ul><li><a href="' + url + ' "><i class="fa ' + iconclass + '"></i>' + text + '</a></li></ul>');
-              
-               
+
+                $('.left').append('<ul><li><a href="' + url + ' "><i class="fa ' + iconclass + '"></i>' + text + '</a></li></ul>');
+
+
 
             }
         }
@@ -64,7 +64,7 @@ function user() {
         type: 'GET',
         dataType: 'json',
         success: function (data) {
-         
+
             var j;
             var dataSet = [];
             var properties = [];
@@ -89,23 +89,45 @@ function user() {
 
 
     $.ajax({
-        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=OgrenciDevamsizlikListesi_mbllogin&dersYiliID=9D7A115C-5E96-4F6E-B31D-E5710BDA1C97&kisiId=' + kisiid + '&cid=' + cid + '',
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=VeliOgrencileri_mbllogin&dersYiliID=9D7A115C-5E96-4F6E-B31D-E5710BDA1C97&kisiId=' + kisiid + '&cid=' + cid + '&dbn=' + dbn + '',
         type: 'GET',
         dataType: 'json',
         success: function (data) {
             var j;
             var dataSet = [];
             var properties = [];
-            //$('#location').empty();
+            $('#selectNumber').empty();
             for (var j = 0; j < data.length; j++) {
-                var text = data[j].Tarih;
-                var kod = data[j].DevamsizlikAdi;
-                var value = data[j].GunKarsiligi;
-
-                $('#example').append('<tr><td>' + text + '</td><td>' + value + '</td><td>' + kod + '</td></tr>');
+                var text = data[j].AdiSoyadi;
+                var value = data[j].OgrenciID;
+                $('#selectNumber').append("<option value=" + value + ">" + text + "</option>");
             }
+            $("#selectNumber").on('change', function () {
+                var ogrenciidselected = this.value;
+                alert(ogrenciidselected);
+                $.ajax({
+                    url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=OgrenciDevamsizlikListesi_mbllogin&dersYiliID=9D7A115C-5E96-4F6E-B31D-E5710BDA1C97&kisiId=' + this.value + '&cid=' + cid + '&dbn=' + dbn + '',
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function (data) {
 
+
+
+                        var j;
+                        var dataSet = [];
+                        var properties = [];
+                        for (var j = 0; j < data.length; j++) {
+                            var text = data[j].Tarih;
+                            var kod = data[j].DevamsizlikAdi;
+                            var value = data[j].GunKarsiligi;
+                            $('#example').append('<tr><td>' + text + '</td><td>' + value + '</td><td>' + kod + '</td></tr>');
+                        }
+                    }
+
+                });
+            });
         }
+
     });
     //Contenier Son
 };
--- OkulSiS_Mobile/OkulSiS_Mobile/www/scripts/devamsizlik/yonetici.js
+++ OkulSiS_Mobile/OkulSiS_Mobile/www/scripts/devamsizlik/yonetici.js
@@ -11,7 +11,7 @@ function user() {
             $(this).children('i:last-child').toggleClass('fa-caret-down fa-caret-left');
         }
     });
- 
+
     var okulid = localStorage.getItem("OkulID");
     var kisiid = localStorage.getItem("gelenid");
     var dersyiliid = localStorage.getItem("dyiliid");
@@ -21,8 +21,9 @@ function user() {
     var rolid = localStorage.getItem("RolID");
     var ip = localStorage.getItem("proxy");
     var kisiadi = localStorage.getItem("KullaniciAdi");
-   
-   
+    document.getElementById('myDate').valueAsDate = new Date();
+
+
 
     //menu başlangıç
 
@@ -32,7 +33,7 @@ function user() {
         type: 'GET',
         dataType: 'json',
         success: function (data) {
-           
+
             var j;
             var len = data.length;
             var dataSet = [];
@@ -47,11 +48,11 @@ function user() {
                 iconclass = data[j].iconclass;
                 collapse = data[j].collapse;
                 // alert(collapse);
-           
 
-                    $('.left').append('<ul><li><a href="' + url + ' "><i class="fa ' + iconclass + '"></i>' + text + '</a></li></ul>');
-              
-               
+
+                $('.left').append('<ul><li><a href="' + url + ' "><i class="fa ' + iconclass + '"></i>' + text + '</a></li></ul>');
+
+
 
             }
         }
@@ -64,7 +65,7 @@ function user() {
         type: 'GET',
         dataType: 'json',
         success: function (data) {
-         
+
             var j;
             var dataSet = [];
             var properties = [];
@@ -86,10 +87,11 @@ function user() {
     // Dashboard son
 
     //contenier başlangıç
-
+    var x = document.getElementById("myDate").value;
 
     $.ajax({
-        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=OgrenciDevamsizlikListesi_mbllogin&dersYiliID=9D7A115C-5E96-4F6E-B31D-E5710BDA1C97&kisiId=' + kisiid + '&cid=' + cid + '',
+
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=KurumVePersonelDevamsizlik_mbllogin&dersYiliID=9D7A115C-5E96-4F6E-B31D-E5710BDA1C97&tarih='+x+'&cid='+cid+'',
         type: 'GET',
         dataType: 'json',
         success: function (data) {
@@ -98,11 +100,13 @@ function user() {
             var properties = [];
             //$('#location').empty();
             for (var j = 0; j < data.length; j++) {
-                var text = data[j].Tarih;
-                var kod = data[j].DevamsizlikAdi;
-                var value = data[j].GunKarsiligi;
-
-                $('#example').append('<tr><td>' + text + '</td><td>' + value + '</td><td>' + kod + '</td></tr>');
+                var Numarasi = data[j].Numarasi;
+                var Adi = data[j].Adsoyad;
+                var SoyAdi = data[j].Soyadi;
+                var Tc = data[j].TCKimlikNo;
+                var aciklama = data[j].DevamsizlikAdi;
+                var selected = data[j].selected;
+                $('#example').append('<tr><td>' + Numarasi + '</td><td>' + Adi + '</td><td>' + aciklama + '</td></tr>');
             }
 
         }
