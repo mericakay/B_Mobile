b80f54a0132ae40b32bf56569c94d523dd5f7529
mericakay <mericakay@gmail.com>
2017-11-21 17:43:49 +0300

    Muhasabe all page finsh

---
+++ OkulSiS_Mobile/OkulSiS_Mobile/www/scripts/ogrenci_listesi/ogrenci_listesi.js
@@ -0,0 +1,94 @@
+﻿
+function user() {
+    $("#showmenu").click(function (e) {
+        e.preventDefault();
+        $("#menu").toggleClass("show");
+    });
+    $("#menu a").click(function (event) {
+        event.preventDefault();
+        if ($(this).next('ul').length - 1) {
+            $(this).next().toggle('fast');
+            $(this).children('i:last-child').toggleClass('fa-caret-down fa-caret-left');
+        }
+    });
+
+    var okulid = localStorage.getItem("OkulID");
+    var kisiid = localStorage.getItem("gelenid");
+    var dersyiliid = localStorage.getItem("dyiliid");
+    var cid = localStorage.getItem("cid");
+    var dbn = localStorage.getItem("dbn");
+    var did = localStorage.getItem("dyiliid");
+    var rolid = localStorage.getItem("RolID");
+    var ip = localStorage.getItem("proxy");
+    var kisiadi = localStorage.getItem("KullaniciAdi");
+    //menu başlangıç
+    $.ajax({
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=mobilMenu_mbllogin&RolID=' + rolid + '&cid=' + cid + '',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+
+            var j;
+            var len = data.length;
+            var dataSet = [];
+            var properties = [];
+            var url = "";
+            var value = "";
+            var iconclass = "";
+            for (var j = 0; j < data.length; j++) {
+                text = data[j].MenuAdi;
+                url = data[j].URL;
+                value = data[j].value;
+                iconclass = data[j].iconclass;
+                collapse = data[j].collapse;
+                // alert(collapse);
+
+
+                $('.left').append('<ul><li><a href="' + url + ' "><i class="fa ' + iconclass + '"></i>' + text + '</a></li></ul>');
+
+
+
+            }
+        }
+    });
+    //menu Son
+    //contenier başlangıç
+    $.ajax({
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=KurumPersoneliSinifListesi_mbllogin&dersYiliID=0F17DCF7-EFCF-41D8-82A0-D4CCFF77E487&cid=1',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var j;
+            var dataSet = [];
+            var properties = [];
+            $('#sube').empty();
+            for (var j = 0; j < data.length; j++) {
+                var text = data[j].SinifAdi;
+                var value = data[j].OgrenciID;
+                $('#sube').append("<option value=" + value + ">" + text + "</option>");
+            }
+            $("#sube").on('change', function () {
+                $.ajax({
+                    url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=Kysubeogrencilistesi_mbllogin&sinifID=F4201B97-B073-4DD7-8891-8091C3DC82CF&cid=1',
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function (data) {
+                        var j;
+                        var dataSet = [];
+                        var properties = [];
+                        for (var j = 0; j < data.length; j++) {
+                            var text = data[j].Aciklama;
+                            var tc = data[j].TCKimlikNo;
+                            var no = data[j].Numarasi;
+                            $('#example').append('<tr><td>' + text + '</td><td>' + tc + '</td><td>' + no + '</td></tr>');
+                        }
+                    }
+
+                });
+            });
+        }
+
+    });
+    //Contenier Son
+};
+
