e33d0d5f7d66e278c79f0542eb6bae757e138543
mericakay <mericakay@gmail.com>
2017-11-27 22:16:31 +0300

    KARNE YAKIN TAMAM

---
+++ OkulSiS_Mobile/OkulSiS_Mobile/www/scripts/karne/yakin.js
@@ -0,0 +1,111 @@
+﻿
+function user() {
+    $("#showmenu").click(function (e) {
+        e.preventDefault();
+        $("#menu").toggleClass("show");
+    });
+    $("#menu a").click(function (event) {
+        event.preventDefault();
+        if ($(this).next('ul').length - 1) {
+            $(this).next().toggle('fast');
+            $(this).children('i:last-child').toggleClass('fa-caret-down fa-caret-left');
+        }
+    });
+
+    var okulid = localStorage.getItem("OkulID");
+    var kisiid = localStorage.getItem("gelenid");
+    var dersyiliid = localStorage.getItem("dyiliid");
+    var cid = localStorage.getItem("cid");
+    var dbn = localStorage.getItem("dbn");
+    var did = localStorage.getItem("dyiliid");
+    var rolid = localStorage.getItem("RolID");
+    var ip = localStorage.getItem("proxy");
+    var kisiadi = localStorage.getItem("KullaniciAdi");
+
+
+
+    //menu başlangıç
+
+
+    $.ajax({
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=mobilMenu_mbllogin&RolID=' + rolid + '&cid=' + cid + '',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+
+            var j;
+            var len = data.length;
+            var dataSet = [];
+            var properties = [];
+            var url = "";
+            var value = "";
+            var iconclass = "";
+            for (var j = 0; j < data.length; j++) {
+                text = data[j].MenuAdi;
+                url = data[j].URL;
+                value = data[j].value;
+                iconclass = data[j].iconclass;
+                collapse = data[j].collapse;
+                // alert(collapse);
+
+
+                $('.left').append('<ul><li><a href="' + url + ' "><i class="fa ' + iconclass + '"></i>' + text + '</a></li></ul>');
+
+
+
+            }
+        }
+    });
+    //menu Son
+
+
+    //contenier başlangıç
+    $.ajax({
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=VeliOgrencileri_mbllogin&dersYiliID=0F17DCF7-EFCF-41D8-82A0-D4CCFF77E487&kisiId=5B153648-7B6B-4160-9274-B2EA69A1D717&cid=' + cid + '',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var j;
+            var dataSet = [];
+            var properties = [];
+            $('#sube').empty();
+            for (var j = 0; j < data.length; j++) {
+                var text = data[j].AdiSoyadi;
+                var sinifid = data[j].SinifID;
+
+                $('#sube').append("<option value=" + sinifid + ">" + text + "</option>");
+            }
+            $("#sube").on('change', function () {
+                $.ajax({
+                    url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=OgrenciKarnesi_mbllogin&donemID=1&ogrenciID=AEEFE2B7-6653-4776-9343-031155AF6181&cid=1',
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function (data) {
+                        var j;
+                        var dataSet = [];
+                        var properties = [];
+                        //$('#location').empty();
+                        for (var j = 0; j < data.length; j++) {
+                            var dersadi = data[j].DersAdi;
+                            var hs = data[j].HaftalikDersSaati;
+                            var ysp = data[j].YilSonuPuani;
+                            var y1 = data[j].Yazili1;
+                            var y2 = data[j].Yazili2;
+                            var y3 = data[j].Yazili3;
+                            var y4 = data[j].Yazili4;
+                            var y5 = data[j].Yazili5;
+
+                            $('#example').append('<tr><td>' + dersadi + '</td><td>' + y1 + '</td><td>' + y2 + '</td><td>' + y3 + '</td><td>' + ysp + '</td></tr>');
+                        }
+
+                    }
+                });
+
+            });
+        }
+    });
+
+   
+    //Contenier Son
+};
+
