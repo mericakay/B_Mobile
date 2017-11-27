d698ae62da598ced2a9c27fecceb7d35d4db73b8
mericakay <mericakay@gmail.com>
2017-11-28 02:25:30 +0300

    gelen mesaj detay tamamlandı

--- OkulSiS_Mobile/OkulSiS_Mobile/www/scripts/mesajlar/mesajlar.js
+++ OkulSiS_Mobile/OkulSiS_Mobile/www/scripts/mesajlar/mesajlar.js
@@ -204,11 +204,35 @@ function user() {
                 var tarih = data[j].Tarih;
                 var Adi = data[j].SenderAdi;
                 var konu = data[j].Konu;
-                var mesaj = data[j].Mesaj;
-                var selected = data[j].selected;
+                var mesaj = data[j].Mesaj;              
 
-                $('#example').append('<tr><td>' + Adi + '</td><td>' + konu + '</td><td>' + tarih + '</td></tr>');
+                $('#example').append('<tr value="' + mesaj + '" ><td  >' + Adi + '</td><td>' + konu + '</td><td>' + tarih + '</td><td style="display:none;">' + mesaj + '</td></tr>');
             }
+            $("#example").on('click', function () {
+
+                var table = document.getElementById("example");
+                var rows = table.getElementsByTagName("tr");
+                for (i = 0; i < rows.length; i++) {
+                    var currentRow = table.rows[i];
+                    var createClickHandler =
+                        function (row) {
+                            return function () {
+                                var rows = $("#location>tr");
+                                // alert(JSON.stringify(rows, null, 4));
+                                console.log(JSON.stringify(rows, null, 4));
+                                var cell = row.getElementsByTagName("td")[3];
+                                
+                                var id = cell.innerHTML;
+                              
+                                alert(id);
+                                // alert("<OgrenciID>" + id + "</OgrenciID>" + "<DevamsizlikKodID>" + gelen + "</DevamsizlikKodID>");
+                            };
+                        };
+
+                    currentRow.onclick = createClickHandler(currentRow);
+                }
+
+            });
 
         }
     });
